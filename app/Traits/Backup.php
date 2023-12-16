a28a43de5f4f54efe5e75b316f49f856f5bccc75
atsugula <111021816+atsugula@users.noreply.github.com>
2023-12-16 00:08:13 -0500

    fix demo

--- app/Traits/Backup.php
+++ app/Traits/Backup.php
@@ -2,18 +2,18 @@
 
 namespace App\Traits;
 
+use \Illuminate\Support\Facades\Log;
+
 trait Backup
 {
 
     public static function generar(){
 
         // Variables para la conexion a la base de datos, mysqldump
-        dd([
-            'DB_HOST' => env('DB_HOST', 'localhost'),
-            'DB_DATABASE' => env('DB_DATABASE', 'serviego'),
-            'DB_USERNAME' => env('DB_USERNAME', 'root'),
-            'DB_PASSWORD' => env('DB_PASSWORD', ''),
-        ]);
+        $db_host=env('DB_HOST', 'localhost');
+        $db_name=env('DB_DATABASE', 'serviego');
+        $db_user=env('DB_USERNAME', 'root');
+        $db_password=env('DB_PASSWORD', '');
 
         // Para identificar nuestro respaldo
         $fecha = date('Ymd-His');
@@ -28,7 +28,15 @@ trait Backup
         $dump = "mysqldump -h $db_host -u $db_user --password=$db_password --opt $db_name > $salida_sql";
 
         // Ejecutamos nuestra instruccion
-        system($dump, $output);
+        // system($dump, $output);
+        exec($dump, $output, $resultCode);
+
+        // Manejo de errores
+        if ($resultCode !== 0) {
+            // Ocurrió un error
+            Log::error("Error al generar el respaldo: $resultCode");
+            return "Error al generar el respaldo: $resultCode";
+        }
 
         return $output;
 
